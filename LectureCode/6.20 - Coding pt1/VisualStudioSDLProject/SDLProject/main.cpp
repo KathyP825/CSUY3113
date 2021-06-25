e4294bfa212eadb57b5aafae1e24400dec8664f7
KathyP825 <kp2327@nyu.edu>
2021-06-24 21:49:53 -0400

    Week 6 notes pt14 -- code pt12

---
+++ LectureCode/6.20 - Coding pt1/VisualStudioSDLProject/SDLProject/main.cpp
@@ -0,0 +1,283 @@
+/*
+//////// CHANGES FOR THIS SAVE ////////
+-   Entity.h -- initialize isActive as true
+-   Entity.cpp -- Update(), Render(), CheckCollision() adds condition to don't bother to do anything if not active
+
+-   tests middle platform with isActive set to false -- works
+
+*/
+
+
+#define GL_SILENCE_DEPRECATION
+
+#ifdef _WINDOWS
+#include <GL/glew.h>
+#endif
+
+#define GL_GLEXT_PROTOTYPES 1
+#include <SDL.h>
+#include <SDL_opengl.h>
+#include "glm/mat4x4.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "ShaderProgram.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#include "Entity.h"
+
+//#define PLATFORM_COUNT 3    // 6.8 -- 3 platforms
+//#define PLATFORM_COUNT 4    // 6.13 -- 4 platforms
+#define PLATFORM_COUNT 5    // 6.14 -- 5 platforms
+
+
+struct GameState {
+    Entity* player;
+    Entity* platforms;
+};
+
+GameState state;
+
+SDL_Window* displayWindow;
+bool gameIsRunning = true;
+
+ShaderProgram program;
+glm::mat4 viewMatrix, modelMatrix, projectionMatrix;
+
+GLuint LoadTexture(const char* filePath) {
+    int w, h, n;
+    unsigned char* image = stbi_load(filePath, &w, &h, &n, STBI_rgb_alpha);
+
+    if (image == NULL) {
+        std::cout << "Unable to load image. Make sure the path is correct\n";
+        assert(false);
+    }
+
+    GLuint textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    stbi_image_free(image);
+    return textureID;
+}
+
+
+
+void Initialize() {
+    SDL_Init(SDL_INIT_VIDEO);
+    displayWindow = SDL_CreateWindow("Textured!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL);
+    SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
+    SDL_GL_MakeCurrent(displayWindow, context);
+
+#ifdef _WINDOWS
+    glewInit();
+#endif
+
+    glViewport(0, 0, 640, 480);
+
+    program.Load("shaders/vertex_textured.glsl", "shaders/fragment_textured.glsl");
+
+    viewMatrix = glm::mat4(1.0f);
+    modelMatrix = glm::mat4(1.0f);
+    projectionMatrix = glm::ortho(-5.0f, 5.0f, -3.75f, 3.75f, -1.0f, 1.0f);
+
+    program.SetProjectionMatrix(projectionMatrix);
+    program.SetViewMatrix(viewMatrix);
+
+    glUseProgram(program.programID);
+
+    glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+    glEnable(GL_BLEND);
+
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+
+    // Initialize Game Objects
+
+    // Initialize Player
+    state.player = new Entity();
+    state.player->position = glm::vec3(0);
+    state.player->movement = glm::vec3(0);
+    state.player->acceleration = glm::vec3(0, -9.81f, 0);   // 6.6 -- set acceleration and never changing this value
+    //state.player->speed = 1.0f;
+    state.player->speed = 1.5f;     // 6.13 -- increase player speed
+    state.player->textureID = LoadTexture("george_0.png");
+
+    state.player->animRight = new int[4]{ 3, 7, 11, 15 };
+    state.player->animLeft = new int[4]{ 1, 5, 9, 13 };
+    state.player->animUp = new int[4]{ 2, 6, 10, 14 };
+    state.player->animDown = new int[4]{ 0, 4, 8, 12 };
+
+    state.player->animIndices = state.player->animRight;
+    state.player->animFrames = 4;
+    state.player->animIndex = 0;
+    state.player->animTime = 0;
+    state.player->animCols = 4;
+    state.player->animRows = 4;
+
+    //6.11 -- fix character hovering
+    state.player->height = 0.8f;    // num can vary
+    state.player->width = 0.8f;     // not necessary, but can if you want
+
+
+    // 6.12 -- set up for jump
+    state.player->jumpPower = 5.0f;
+
+
+    // 6.8 -- set up platforms
+    state.platforms = new Entity[PLATFORM_COUNT];
+    GLuint platformTextureID = LoadTexture("platformPack_tile001.png");
+    
+    state.platforms[0].textureID = platformTextureID;
+    state.platforms[0].position = glm::vec3(-1, -3.25f, 0);
+
+    state.platforms[1].textureID = platformTextureID;
+    state.platforms[1].position = glm::vec3(0, -3.25f, 0);
+    //state.platforms[1].isActive = false;      // 6.20 -- test for platform's isActive is false
+
+    state.platforms[2].textureID = platformTextureID;
+    state.platforms[2].position = glm::vec3(1, -3.25f, 0);
+
+    state.platforms[3].textureID = platformTextureID;       // 6.13 -- add 4th platform
+    state.platforms[3].position = glm::vec3(-3, -3.25f, 0);
+
+    state.platforms[4].textureID = platformTextureID;       // 6.14 -- add 5th platform
+    state.platforms[4].position = glm::vec3(1.2, -2.25f, 0);
+
+    for (size_t i = 0; i < PLATFORM_COUNT; i++) {   // only updates platforms 1x
+        //state.platforms[i].Update(0);     // 6.9 -- update to version with all parameters
+        state.platforms[i].Update(0, NULL, 0);
+    }
+
+}
+
+
+
+void ProcessInput() {
+
+    state.player->movement = glm::vec3(0);
+
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+        switch (event.type) {
+        case SDL_QUIT:
+        case SDL_WINDOWEVENT_CLOSE:
+            gameIsRunning = false;
+            break;
+
+        case SDL_KEYDOWN:
+            switch (event.key.keysym.sym) {
+            case SDLK_LEFT:
+                // Move the player left
+                break;
+
+            case SDLK_RIGHT:
+                // Move the player right
+                break;
+
+            case SDLK_SPACE:
+                // jump
+                state.player->jump = true;  // 6.12
+                break;
+            }
+            break; // SDL_KEYDOWN
+        }
+    }
+
+    const Uint8* keys = SDL_GetKeyboardState(NULL);
+
+    if (keys[SDL_SCANCODE_LEFT]) {
+        state.player->movement.x = -1.0f;
+        state.player->animIndices = state.player->animLeft;
+    }
+    else if (keys[SDL_SCANCODE_RIGHT]) {
+        state.player->movement.x = 1.0f;
+        state.player->animIndices = state.player->animRight;
+    }
+
+
+    if (glm::length(state.player->movement) > 1.0f) {
+        state.player->movement = glm::normalize(state.player->movement);
+    }
+
+}
+
+
+
+/*
+float lastTicks = 0.0f;
+void Update() {
+    float ticks = (float)SDL_GetTicks() / 1000.0f;
+    float deltaTime = ticks - lastTicks;
+    lastTicks = ticks;
+
+    state.player->Update(deltaTime);
+}
+*/
+
+// 6.5 -- replace Update() to fixed timestate version
+#define FIXED_TIMESTEP 0.0166666f   // 60 times per second
+float lastTicks = 0;
+float accumulator = 0.0f;
+
+void Update() {
+    float ticks = (float)SDL_GetTicks() / 1000.0f;
+    float deltaTime = ticks - lastTicks;
+    lastTicks = ticks;
+
+    deltaTime += accumulator;
+    if (deltaTime < FIXED_TIMESTEP) {
+        // if not enough time has passed, store into accumulator and don't do anything
+        accumulator = deltaTime;
+        return;
+    }
+
+    // when enough time has passed, update player with FIXED_TIMESTEP
+    // if have extra time (accumulate 2x), update 2x
+    while (deltaTime >= FIXED_TIMESTEP) {
+        // Update. Notice it's FIXED_TIMESTEP. Not deltaTime
+        //state.player->Update(FIXED_TIMESTEP);     // 6.9 -- update to include all parameters
+        state.player->Update(FIXED_TIMESTEP, state.platforms, PLATFORM_COUNT);
+        deltaTime -= FIXED_TIMESTEP;
+    }
+    accumulator = deltaTime;
+}
+
+
+
+
+void Render() {
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // render and display all platforms
+    for (size_t i = 0; i < PLATFORM_COUNT; i++) {
+        state.platforms[i].Render(&program);
+    }
+
+
+    state.player->Render(&program);
+
+    SDL_GL_SwapWindow(displayWindow);
+}
+
+
+void Shutdown() {
+    SDL_Quit();
+}
+
+int main(int argc, char* argv[]) {
+    Initialize();
+
+    while (gameIsRunning) {
+        ProcessInput();
+        Update();
+        Render();
+    }
+
+    Shutdown();
+    return 0;
+}
