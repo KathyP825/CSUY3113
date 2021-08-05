7812152c3ae72a501523c4d44729bf71eef05c9b
KathyP825 <kp2327@nyu.edu>
2021-08-04 23:51:29 -0400

    implemented Scene success, moved level code into Level1

---
+++ P6/VisualStudioSDLProject/SDLProject/Level1.cpp
@@ -0,0 +1,157 @@
+#include "Level1.h"
+
+#define OBJECT_COUNT 6
+#define ENEMY_COUNT 1
+
+
+void Level1::Initialize() {
+    state.nextScene = -1;
+
+    /*
+    -----------------   Initialize Player  -----------------
+    */
+    state.player = new Entity();
+    state.player->entityType = PLAYER;
+    state.player->position = glm::vec3(0, 0.75f, 0);
+    state.player->acceleration = glm::vec3(0, 0, 0);
+    state.player->speed = 2.0f;
+
+    /*
+    -----------------   Initialize Objects  -----------------
+    */
+    state.objects = new Entity[OBJECT_COUNT];
+    
+    // ----- FLOOR -----
+    GLuint floorTextureID = Util::LoadTexture("grasstext.jpg");
+    Mesh* cubeMesh = new Mesh();
+    cubeMesh->LoadOBJ("cube.obj", 30);  // 12.3 -- duplicate texture 10 times // 12.6 -- dup 20 times
+
+    state.objects[0].textureID = floorTextureID;
+    state.objects[0].mesh = cubeMesh;
+    state.objects[0].position = glm::vec3(0.0f, -0.25f, 0.0f);
+    state.objects[0].rotation = glm::vec3(0.0f, 0.0f, 0.0f);
+    state.objects[0].acceleration = glm::vec3(0.0f, 0.0f, 0.0f);
+    state.objects[0].scale = glm::vec3(30.0f, 0.5f, 30.0f);
+    state.objects[0].entityType = FLOOR;
+
+    // ----- CEILING -----
+    GLuint ceilingTextureID = Util::LoadTexture("bricktext.jpg");
+    Mesh* ceilingMesh = new Mesh();
+    ceilingMesh->LoadOBJ("cube.obj", 30);
+
+    state.objects[1].textureID = ceilingTextureID;
+    state.objects[1].mesh = ceilingMesh;
+    state.objects[1].position = glm::vec3(0.0f, 3.75f, 0.0f);
+    state.objects[1].rotation = glm::vec3(0.0f, 0.0f, 0.0f);
+    state.objects[1].acceleration = glm::vec3(0.0f, 0.0f, 0.0f);
+    state.objects[1].scale = glm::vec3(30.0f, 0.5f, 30.0f);
+    state.objects[1].entityType = CEILING;
+
+
+    // ----- WALLS ----- 
+    // i = [1] to [4]
+    GLuint wallTextureID = Util::LoadTexture("sampleWall.jpg");
+    Mesh* wallMesh = new Mesh();
+    wallMesh->LoadOBJ("cube.obj", 1);  // 12.3 -- duplicate texture 10 times // 12.6 -- dup 20 times
+
+    for (size_t i = 2; i < OBJECT_COUNT; i++) {
+        state.objects[i].textureID = wallTextureID;
+        state.objects[i].mesh = wallMesh;
+
+        state.objects[i].acceleration = glm::vec3(0.0f, 0.0f, 0.0f);
+        state.objects[1].rotation = glm::vec3(0.0f, 0.0f, 0.0f);
+        state.objects[i].entityType = WALL;
+    }
+
+    state.objects[2].scale = glm::vec3(0.5f, 4.0f, 30.0f);
+    state.objects[2].position = glm::vec3(15.0f, 1.5f, 0.0f);
+    state.objects[2].width = 0.5f;
+    state.objects[2].height = 4.0f;
+    state.objects[2].depth = 30.0f;
+
+    state.objects[3].scale = glm::vec3(0.5f, 4.0f, 30.0f);
+    state.objects[3].position = glm::vec3(-15.0f, 1.5f, 0.0f);
+    state.objects[3].width = 0.5f;
+    state.objects[3].height = 4.0f;
+    state.objects[3].depth = 30.0f;
+
+    state.objects[4].scale = glm::vec3(30.0f, 4.0f, 0.5f);
+    state.objects[4].position = glm::vec3(0.0f, 1.5f, 15.0f);
+    state.objects[4].width = 30.0f;
+    state.objects[4].height = 4.0f;
+    state.objects[4].depth = 0.5f;
+
+    state.objects[5].scale = glm::vec3(30.0f, 4.0f, 0.5f);
+    state.objects[5].position = glm::vec3(0.0f, 1.5f, -15.0f);
+    state.objects[5].width = 30.0f;
+    state.objects[5].height = 4.0f;
+    state.objects[5].depth = 0.5f;
+
+    // CRATES
+    //GLuint crateTextureID = Util::LoadTexture("crate1_diffuse.png");
+    //Mesh* crateMesh = new Mesh();   // 12.6 -- need a new one b/c floor one is duplicated
+    //crateMesh->LoadOBJ("cube.obj", 1);  // 12.6 -- 1 = no duplicate
+
+    //state.objects[1].textureID = crateTextureID;
+    //state.objects[1].mesh = crateMesh;
+    //state.objects[1].position = glm::vec3(0.0f, 0.5f, -5.0f);   // 12.6 -- crate is 1 unit tall, stop from sinking into floor
+    //state.objects[1].entityType = CRATE;
+
+    //state.objects[2].textureID = crateTextureID;
+    //state.objects[2].mesh = crateMesh;
+    //state.objects[2].position = glm::vec3(-1.0f, 0.5f, -5.0f);   // 12.7 -- next to 1st crate
+    //state.objects[2].entityType = CRATE;
+
+    //state.objects[3].textureID = crateTextureID;
+    //state.objects[3].mesh = crateMesh;
+    //state.objects[3].position = glm::vec3(0.0f, 1.5f, -5.0f);   // 12.7 -- on top of 1st crate
+    //state.objects[3].entityType = CRATE;
+
+    /*
+    -----------------   Initialize Enemies  -----------------
+    */
+    // 12.12
+    state.enemies = new Entity[ENEMY_COUNT];
+    GLuint enemyTextureID = Util::LoadTexture("ctg.png");
+
+    for (int i = 0; i < ENEMY_COUNT; i++) {
+        state.enemies[i].billboard = true;
+        state.enemies[i].textureID = enemyTextureID;
+        state.enemies[i].position = glm::vec3(rand() % 20 - 10, 0.5, rand() % 20 - 10); // 0.5 = edit to have enemies touch ground
+        state.enemies[i].rotation = glm::vec3(0, 0, 0);
+        state.enemies[i].acceleration = glm::vec3(0, 0, 0);
+        state.enemies[i].speed = 1.0f;
+        //state.enemies[i].isActive = false;
+    }
+    
+}
+
+
+void Level1::Update(float deltaTime) {
+    //
+    state.player->Update(deltaTime, state.player, state.objects, OBJECT_COUNT);
+    
+    // 11.7 -- update cube
+    for (size_t i = 0; i < OBJECT_COUNT; i++) {
+        state.objects[i].Update(deltaTime, state.player, state.objects, OBJECT_COUNT);
+    }
+
+    // 12.13 -- update enemies
+    for (size_t i = 0; i < ENEMY_COUNT; i++) {
+        state.enemies[i].Update(deltaTime, state.player, state.objects, OBJECT_COUNT);
+    }
+
+}
+
+
+void Level1::Render(ShaderProgram* program) {
+    // 11.7 -- render objects
+    for (size_t i = 0; i < OBJECT_COUNT; i++) {
+        state.objects[i].Render(program);
+    }
+
+    // 12.13 -- render enemies
+    for (size_t i = 0; i < ENEMY_COUNT; i++) {
+        state.enemies[i].Render(program);
+    }
+}
--- P6/VisualStudioSDLProject/SDLProject/main.cpp
+++ P6/VisualStudioSDLProject/SDLProject/main.cpp
@@ -15,6 +15,14 @@
 
 #include "Util.h"
 #include "Entity.h"
+#include "Scene.h"
+
+//#include "Menu.h"
+#include "Level1.h"
+
+
+#define OBJECT_COUNT 6
+#define ENEMY_COUNT 1
 
 
 SDL_Window* displayWindow;
@@ -23,23 +31,21 @@ bool gameIsRunning = true;
 ShaderProgram program;
 glm::mat4 viewMatrix, modelMatrix, projectionMatrix;
 
-// 11.7
-#define OBJECT_COUNT 6
+Scene* currentScene;
+Scene* sceneList[1];    // only handle game level for now
 
-// 12.12 -- enemies
-#define ENEMY_COUNT 1
+// for audio
+//Mix_Music* music;
+//Mix_Chunk* sound;
 
+void SwitchToScene(Scene* scene) {
+    currentScene = scene;
+    currentScene->Initialize();
+}
 
-struct GameState {
-    Entity* player;
-    Entity* objects;    // 11.7
-    Entity* enemies;    // 12.12
-};
-
-GameState state;
 
 void Initialize() {
-    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Init(SDL_INIT_VIDEO);   // need to add audio pipe
     displayWindow = SDL_CreateWindow("3D!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_OPENGL);  // 11.5 -- change window size
     SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
     SDL_GL_MakeCurrent(displayWindow, context);
@@ -71,139 +77,18 @@ void Initialize() {
     glDepthMask(GL_TRUE);
     glDepthFunc(GL_LEQUAL);
 
-
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
-    /*
-    -----------------   Initialize Player  -----------------
-    */
-
-    state.player = new Entity();
-    state.player->entityType = PLAYER;
-    state.player->position = glm::vec3(0, 0.75f, 0);
-    state.player->acceleration = glm::vec3(0, 0, 0);
-    state.player->speed = 1.0f;
-
-
-    /*
-    -----------------   Initialize Objects  -----------------
-    */
-    state.objects = new Entity[OBJECT_COUNT];
-    
-    // 12.2 -- remove commented out code, delete ships, replace with floor
-    // 11.14 -- comment out cube, Mario, and Pikachu -- replace with ship
-    GLuint floorTextureID = Util::LoadTexture("grasstext.jpg");
-    Mesh* cubeMesh = new Mesh();
-    //cubeMesh->LoadOBJ("cube.obj");
-    cubeMesh->LoadOBJ("cube.obj", 30);  // 12.3 -- duplicate texture 10 times // 12.6 -- dup 20 times
-
-    state.objects[0].textureID = floorTextureID;
-    state.objects[0].mesh = cubeMesh;
-    state.objects[0].position = glm::vec3(0.0f, -0.25f, 0.0f);
-    state.objects[0].rotation = glm::vec3(0.0f, 0.0f, 0.0f);
-    state.objects[0].acceleration = glm::vec3(0.0f, 0.0f, 0.0f);
-    state.objects[0].scale = glm::vec3(30.0f, 0.5f, 30.0f);
-    state.objects[0].entityType = FLOOR;
-
-
-    // ceiling
-    GLuint ceilingTextureID = Util::LoadTexture("bricktext.jpg");
-    Mesh* ceilingMesh = new Mesh();
-    //cubeMesh->LoadOBJ("cube.obj");
-    ceilingMesh->LoadOBJ("cube.obj", 30);  // 12.3 -- duplicate texture 10 times // 12.6 -- dup 20 times
-
-    state.objects[1].textureID = ceilingTextureID;
-    state.objects[1].mesh = ceilingMesh;
-    state.objects[1].position = glm::vec3(0.0f, 3.75f, 0.0f);
-    state.objects[1].rotation = glm::vec3(0.0f, 0.0f, 0.0f);
-    state.objects[1].acceleration = glm::vec3(0.0f, 0.0f, 0.0f);
-    state.objects[1].scale = glm::vec3(30.0f, 0.5f, 30.0f);
-    state.objects[1].entityType = CEILING;
-
-
-
-    // CRATES
-    //GLuint crateTextureID = Util::LoadTexture("crate1_diffuse.png");
-    //Mesh* crateMesh = new Mesh();   // 12.6 -- need a new one b/c floor one is duplicated
-    //crateMesh->LoadOBJ("cube.obj", 1);  // 12.6 -- 1 = no duplicate
-
-    //state.objects[1].textureID = crateTextureID;
-    //state.objects[1].mesh = crateMesh;
-    //state.objects[1].position = glm::vec3(0.0f, 0.5f, -5.0f);   // 12.6 -- crate is 1 unit tall, stop from sinking into floor
-    //state.objects[1].entityType = CRATE;
-
-    //state.objects[2].textureID = crateTextureID;
-    //state.objects[2].mesh = crateMesh;
-    //state.objects[2].position = glm::vec3(-1.0f, 0.5f, -5.0f);   // 12.7 -- next to 1st crate
-    //state.objects[2].entityType = CRATE;
-
-    //state.objects[3].textureID = crateTextureID;
-    //state.objects[3].mesh = crateMesh;
-    //state.objects[3].position = glm::vec3(0.0f, 1.5f, -5.0f);   // 12.7 -- on top of 1st crate
-    //state.objects[3].entityType = CRATE;
-
-    // Walls, i = [1] to i = [4]
-    GLuint wallTextureID = Util::LoadTexture("sampleWall.jpg");
-    Mesh* wallMesh = new Mesh();
-    wallMesh->LoadOBJ("cube.obj", 1);  // 12.3 -- duplicate texture 10 times // 12.6 -- dup 20 times
-
-    for (size_t i = 2; i < OBJECT_COUNT; i++) {
-        state.objects[i].textureID = wallTextureID;
-        state.objects[i].mesh = wallMesh;
-
-        state.objects[i].acceleration = glm::vec3(0.0f, 0.0f, 0.0f);
-        state.objects[1].rotation = glm::vec3(0.0f, 0.0f, 0.0f);
-        state.objects[i].entityType = WALL;
-    }
-
-    state.objects[2].scale = glm::vec3(0.5f, 4.0f, 30.0f);
-    state.objects[2].position = glm::vec3(15.0f, 1.5f, 0.0f);
-    state.objects[2].width = 0.5f;
-    state.objects[2].height = 4.0f;
-    state.objects[2].depth = 30.0f;
-
-    state.objects[3].scale = glm::vec3(0.5f, 4.0f, 30.0f);
-    state.objects[3].position = glm::vec3(-15.0f, 1.5f, 0.0f);
-    state.objects[3].width = 0.5f;
-    state.objects[3].height = 4.0f;
-    state.objects[3].depth = 30.0f;
-
-    state.objects[4].scale = glm::vec3(30.0f, 4.0f, 0.5f);
-    state.objects[4].position = glm::vec3(0.0f, 1.5f, 15.0f);
-    state.objects[4].width = 30.0f;
-    state.objects[4].height = 4.0f;
-    state.objects[4].depth = 0.5f;
-
-    state.objects[5].scale = glm::vec3(30.0f, 4.0f, 0.5f);
-    state.objects[5].position = glm::vec3(0.0f, 1.5f, -15.0f);
-    state.objects[5].width = 30.0f;
-    state.objects[5].height = 4.0f;
-    state.objects[5].depth = 0.5f;
-
-
-
-
-
-    /*
-    -----------------   Initialize Enemies  -----------------
-    */
-    // 12.12
-    state.enemies = new Entity[ENEMY_COUNT];
-    GLuint enemyTextureID = Util::LoadTexture("ctg.png");
-
-    for (int i = 0; i < ENEMY_COUNT; i++) {
-        state.enemies[i].billboard = true;
-        state.enemies[i].textureID = enemyTextureID;
-        state.enemies[i].position = glm::vec3(rand() % 20 - 10, 0.5, rand() % 20 - 10); // 0.5 = edit to have enemies touch ground
-        state.enemies[i].rotation = glm::vec3(0, 0, 0);
-        state.enemies[i].acceleration = glm::vec3(0, 0, 0);
-        state.enemies[i].isActive = false;
-    }
+    // scenes
+    sceneList[0] = new Level1();
+    SwitchToScene(sceneList[0]);
 
 }
 
 void ProcessInput() {
+    //currentScene->state.player->velocity = glm::vec3(0);
     SDL_Event event;
+
     while (SDL_PollEvent(&event)) {
         switch (event.type) {
         case SDL_QUIT:
@@ -213,6 +98,14 @@ void ProcessInput() {
 
         case SDL_KEYDOWN:
             switch (event.key.keysym.sym) {
+            case SDLK_RETURN:
+                // starts game at Level 1
+                // only works if in Main Menu
+                //if (currentScene == sceneList[0]) {
+                //    SwitchToScene(sceneList[1]);
+                //}
+                break;
+
             case SDLK_SPACE:
                 // Some sort of action
                 break;
@@ -225,22 +118,22 @@ void ProcessInput() {
     // 12.5 -- A and D turns left/right
     const Uint8* keys = SDL_GetKeyboardState(NULL);
     if (keys[SDL_SCANCODE_A]) {
-        //state.player->rotation.y += 1.0f;
-        state.player->rotation.y += 0.1f;   // make slower
+        //currentScene->state.player->rotation.y += 1.0f;
+        currentScene->state.player->rotation.y += 0.1f;   // make slower
     }
     else if (keys[SDL_SCANCODE_D]) {
-        state.player->rotation.y -= 0.1f;   // make slower
+        currentScene->state.player->rotation.y -= 0.1f;   // make slower
     }
 
-    state.player->velocity.x = 0;
-    state.player->velocity.z = 0;
+    currentScene->state.player->velocity.x = 0;
+    currentScene->state.player->velocity.z = 0;
     if (keys[SDL_SCANCODE_W]) {
-        state.player->velocity.z = cos(glm::radians(state.player->rotation.y)) * -2.0f;
-        state.player->velocity.x = sin(glm::radians(state.player->rotation.y)) * -2.0f;
+        currentScene->state.player->velocity.z = cos(glm::radians(currentScene->state.player->rotation.y)) * -2.0f;
+        currentScene->state.player->velocity.x = sin(glm::radians(currentScene->state.player->rotation.y)) * -2.0f;
     }
     else if (keys[SDL_SCANCODE_S]) {
-        state.player->velocity.z = cos(glm::radians(state.player->rotation.y)) * 2.0f;
-        state.player->velocity.x = sin(glm::radians(state.player->rotation.y)) * 2.0f;
+        currentScene->state.player->velocity.z = cos(glm::radians(currentScene->state.player->rotation.y)) * 2.0f;
+        currentScene->state.player->velocity.x = sin(glm::radians(currentScene->state.player->rotation.y)) * 2.0f;
     }
 }
 
@@ -259,18 +152,20 @@ void Update() {
         return;
     }
 
+    // time accumulator
     while (deltaTime >= FIXED_TIMESTEP) {
-        state.player->Update(FIXED_TIMESTEP, state.player, state.objects, OBJECT_COUNT);
+        //currentScene->state.player->Update(FIXED_TIMESTEP, currentScene->state.player, currentScene->state.objects, OBJECT_COUNT);
+        currentScene->Update(FIXED_TIMESTEP);
 
-        // 11.7 -- update cube
-        for (size_t i = 0; i < OBJECT_COUNT; i++) {
-            state.objects[i].Update(FIXED_TIMESTEP, state.player, state.objects, OBJECT_COUNT);
-        }
+        //// 11.7 -- update cube
+        //for (size_t i = 0; i < OBJECT_COUNT; i++) {
+        //    currentScene->state.objects[i].Update(FIXED_TIMESTEP, currentScene->state.player, currentScene->state.objects, OBJECT_COUNT);
+        //}
 
-        // 12.13 -- update enemies
-        for (size_t i = 0; i < ENEMY_COUNT; i++) {
-            state.enemies[i].Update(FIXED_TIMESTEP, state.player, state.objects, OBJECT_COUNT);
-        }
+        //// 12.13 -- update enemies
+        //for (size_t i = 0; i < ENEMY_COUNT; i++) {
+        //    currentScene->state.enemies[i].Update(FIXED_TIMESTEP, currentScene->state.player, currentScene->state.objects, OBJECT_COUNT);
+        //}
 
         deltaTime -= FIXED_TIMESTEP;
     }
@@ -279,8 +174,8 @@ void Update() {
 
     // 12.5 -- update viewMatrix
     viewMatrix = glm::mat4(1.0f);
-    viewMatrix = glm::rotate(viewMatrix, glm::radians(state.player->rotation.y), glm::vec3(0, -1.0f, 0));
-    viewMatrix = glm::translate(viewMatrix, -state.player->position);
+    viewMatrix = glm::rotate(viewMatrix, glm::radians(currentScene->state.player->rotation.y), glm::vec3(0, -1.0f, 0));
+    viewMatrix = glm::translate(viewMatrix, -currentScene->state.player->position);
 }
 
 
@@ -289,15 +184,7 @@ void Render() {
     
     program.SetViewMatrix(viewMatrix);  // 12.5
 
-    // 11.7 -- render objects
-    for (size_t i = 0; i < OBJECT_COUNT; i++) {
-        state.objects[i].Render(&program);
-    }
-
-    // 12.13 -- render enemies
-    for (size_t i = 0; i < ENEMY_COUNT; i++) {
-        state.enemies[i].Render(&program);
-    }
+    currentScene->Render(&program);     // need to render scene
 
     SDL_GL_SwapWindow(displayWindow);
 }
