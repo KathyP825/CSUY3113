d5b409b0872d7d85f8cdef823ee49e325776fc71
KathyP825 <kp2327@nyu.edu>
2021-08-10 00:47:24 -0400

    added maze walls for right, back, and start to top left corner

--- P6/VisualStudioSDLProject/SDLProject/Level1.cpp
+++ P6/VisualStudioSDLProject/SDLProject/Level1.cpp
@@ -1,6 +1,6 @@
 #include "Level1.h"
 
-#define OBJECT_COUNT 31
+#define OBJECT_COUNT 61
 #define ENEMY_COUNT 3
 
 
@@ -12,7 +12,10 @@ void Level1::Initialize() {
     */
     state.player = new Entity();
     state.player->entityType = PLAYER;
-    state.player->position = glm::vec3(0, 0.75f, 0);    // return to 0.75
+    //state.player->position = glm::vec3(0, 0.75f, 0);  // return to this 
+
+    state.player->position = glm::vec3(-5.0, 0.75f, 5.0);    // for testing
+
     state.player->width = 0.5;
     state.player->acceleration = glm::vec3(0, 0, 0);
     state.player->speed = 3.0f;
@@ -119,7 +122,7 @@ void Level1::Initialize() {
     state.objects[7].position = glm::vec3(0.0f, 2.0f, -24.8f);
     state.objects[7].width = 2.0f;
     state.objects[7].height = 4.0f;
-    state.objects[7].depth = 0.2f;
+    state.objects[7].depth = 0.5f;
 
     // ----- Maze Walls ----- 
     GLuint crateTextureID = Util::LoadTexture("crate1_diffuse.png");
@@ -141,11 +144,11 @@ void Level1::Initialize() {
     state.objects[8].height = 1.0f;
     state.objects[8].depth = 0.2f;
 
-    state.objects[9].scale = glm::vec3(0.2f, 1.0f, 5.0f);
-    state.objects[9].position = glm::vec3(2.5f, 0.5f, 0.0f);    // player right
+    state.objects[9].scale = glm::vec3(0.2f, 1.0f, 10.0f);
+    state.objects[9].position = glm::vec3(2.5f, 0.5f, -2.5f);    // player right
     state.objects[9].width = 0.2f;
     state.objects[9].height = 1.0f;
-    state.objects[9].depth = 5.0f;
+    state.objects[9].depth = 10.0f;
 
     state.objects[10].scale = glm::vec3(0.2f, 1.0f, 5.0f);
     state.objects[10].position = glm::vec3(-2.5f, 0.5f, 0.0f);    // player left
@@ -197,11 +200,11 @@ void Level1::Initialize() {
     state.objects[17].height = 1.0f;
     state.objects[17].depth = 0.2f;
 
-    state.objects[18].scale = glm::vec3(0.2f, 1.0f, 6.0f);
-    state.objects[18].position = glm::vec3(6.6f, 0.5f, -18.0f);   // outer wall
+    state.objects[18].scale = glm::vec3(0.2f, 1.0f, 4.0f);
+    state.objects[18].position = glm::vec3(6.6f, 0.5f, -19.05f);   // outer wall, was too long
     state.objects[18].width = 0.2f;
     state.objects[18].height = 1.0f;
-    state.objects[18].depth = 6.0f;
+    state.objects[18].depth = 4.0f;
 
     // Bottom Right corner
     state.objects[19].scale = glm::vec3(0.2f, 1.0f, 5.0f);
@@ -216,15 +219,15 @@ void Level1::Initialize() {
     state.objects[20].height = 1.0f;
     state.objects[20].depth = 0.2f;
 
-    // Top Right corner
+    // Top Left corner
     state.objects[21].scale = glm::vec3(0.2f, 1.0f, 5.0f);
     state.objects[21].position = glm::vec3(-15.0f, 0.5f, -22.5f);   // outer wall
     state.objects[21].width = 0.2f;
     state.objects[21].height = 1.0f;
     state.objects[21].depth = 5.0f;
 
-    state.objects[22].scale = glm::vec3(14.0f, 1.0f, 0.2f);
-    state.objects[22].position = glm::vec3(-19.0f, 0.5f, -15.0f);   // horizontal wall
+    state.objects[22].scale = glm::vec3(16.0f, 1.0f, 0.2f);
+    state.objects[22].position = glm::vec3(-17.0f, 0.5f, -15.0f);   // horizontal wall
     state.objects[22].width = 14.0f;
     state.objects[22].height = 1.0f;
     state.objects[22].depth = 0.2f;
@@ -257,16 +260,17 @@ void Level1::Initialize() {
 
     // Midpoint, left wall between Top Left and Bottom Left
     state.objects[27].scale = glm::vec3(6.0f, 1.0f, 0.2f);
-    state.objects[27].position = glm::vec3(-18.0f, 0.5f, -4.0f);   // horizontal front wall
+    state.objects[27].position = glm::vec3(-18.0f, 0.5f, -6.0f);   // horizontal front wall
     state.objects[27].width = 6.0f;
     state.objects[27].height = 1.0f;
     state.objects[27].depth = 0.2f;
 
-    state.objects[28].scale = glm::vec3(6.0f, 1.0f, 0.2f);
-    state.objects[28].position = glm::vec3(-22.0f, 0.5f, 4.0f);   // horizontal back wall
-    state.objects[28].width = 6.0f;
+    state.objects[28].scale = glm::vec3(4.0f, 1.0f, 0.2f);
+    state.objects[28].position = glm::vec3(-23.0f, 0.5f, 4.0f);   // horizontal back wall, dead end wall
+    state.objects[28].width = 4.0f;
     state.objects[28].height = 1.0f;
     state.objects[28].depth = 0.2f;
+    //state.objects[28].isActive = false;
 
     // Midpoint, back walls between Bottom Left and Bottom Right
     state.objects[29].scale = glm::vec3(0.2f, 1.0f, 6.0f);
@@ -280,6 +284,206 @@ void Level1::Initialize() {
     state.objects[30].width = 0.2f;
     state.objects[30].height = 1.0f;
     state.objects[30].depth = 6.0f;
+
+    // connect Upper Right to Right midpoint
+    state.objects[31].scale = glm::vec3(12.0f, 1.0f, 0.2f);
+    state.objects[31].position = glm::vec3(15.0f, 0.5f, -10.2f);   // horizontal wall
+    state.objects[31].width = 12.0f;
+    state.objects[31].height = 1.0f;
+    state.objects[31].depth = 0.2f;
+
+    state.objects[32].scale = glm::vec3(0.2f, 1.0f, 10.0f);
+    state.objects[32].position = glm::vec3(15.0f, 0.5f, -1.0f);   // inner vertical wall
+    state.objects[32].width = 0.2f;
+    state.objects[32].height = 1.0f;
+    state.objects[32].depth = 10.0f;
+
+    state.objects[33].scale = glm::vec3(0.2f, 1.0f, 9.0f);
+    state.objects[33].position = glm::vec3(9.0f, 0.5f, -5.5f);   // outer vertical wall
+    state.objects[33].width = 0.2f;
+    state.objects[33].height = 1.0f;
+    state.objects[33].depth = 9.0f;
+
+    state.objects[34].scale = glm::vec3(6.0f, 1.0f, 0.2f);
+    state.objects[34].position = glm::vec3(12.0f, 0.5f, -1.0f);   // horizontal wall, dead end, put health near here
+    state.objects[34].width = 6.0f;
+    state.objects[34].height = 1.0f;
+    state.objects[34].depth = 0.2f;
+
+    state.objects[35].scale = glm::vec3(6.0f, 1.0f, 0.2f);
+    state.objects[35].position = glm::vec3(12.0f, 0.5f, 20.0f);   // horizontal wall, connecting lower right
+    state.objects[35].width = 6.0f;
+    state.objects[35].height = 1.0f;
+    state.objects[35].depth = 0.2f;
+
+    state.objects[36].scale = glm::vec3(0.2f, 1.0f, 7.0f);
+    state.objects[36].position = glm::vec3(9.0f, 0.5f, 16.5f);   // outer vertical wall, path to lower right and exit
+    state.objects[36].width = 0.2f;
+    state.objects[36].height = 1.0f;
+    state.objects[36].depth = 7.0f;
+
+    state.objects[37].scale = glm::vec3(0.2f, 1.0f, 6.0f);
+    state.objects[37].position = glm::vec3(21.0f, 0.5f, 7.0f);   // connect vertical wall
+    state.objects[37].width = 0.2f;
+    state.objects[37].height = 1.0f;
+    state.objects[37].depth = 6.0f;
+
+    state.objects[38].scale = glm::vec3(12.0f, 1.0f, 0.2f);
+    state.objects[38].position = glm::vec3(15.0f, 0.5f, 10.0f);   // horizontal wall, to crossroad
+    state.objects[38].width = 12.0f;
+    state.objects[38].height = 1.0f;
+    state.objects[38].depth = 0.2f;
+
+    // replicate on left side
+    state.objects[39].scale = glm::vec3(6.0f, 1.0f, 0.2f);
+    state.objects[39].position = glm::vec3(-12.0f, 0.5f, 20.0f);   // horizontal wall, connecting lower left
+    state.objects[39].width = 6.0f;
+    state.objects[39].height = 1.0f;
+    state.objects[39].depth = 0.2f;
+    //state.objects[39].isActive = false;
+
+    state.objects[40].scale = glm::vec3(0.2f, 1.0f, 7.0f);
+    state.objects[40].position = glm::vec3(-9.0f, 0.5f, 16.5f);   // outer vertical wall, path to lower left 
+    state.objects[40].width = 0.2f;
+    state.objects[40].height = 1.0f;
+    state.objects[40].depth = 7.0f;
+    //state.objects[40].isActive = false;
+
+    state.objects[41].scale = glm::vec3(0.2f, 1.0f, 6.0f);
+    state.objects[41].position = glm::vec3(-21.0f, 0.5f, 7.0f);   // connect vertical wall
+    state.objects[41].width = 0.2f;
+    state.objects[41].height = 1.0f;
+    state.objects[41].depth = 6.0f;
+    //state.objects[41].isActive = false;
+
+    state.objects[42].scale = glm::vec3(12.0f, 1.0f, 0.2f);
+    state.objects[42].position = glm::vec3(-15.0f, 0.5f, 10.0f);   // horizontal wall, to crossroad
+    state.objects[42].width = 12.0f;
+    state.objects[42].height = 1.0f;
+    state.objects[42].depth = 0.2f;
+    //state.objects[42].isActive = false;
+
+    // player path
+    state.objects[43].scale = glm::vec3(10.0f, 1.0f, 0.2f);
+    state.objects[43].position = glm::vec3(-2.5f, 0.5f, -7.5f);   // 1st wall in front of player
+    state.objects[43].width = 10.0f;
+    state.objects[43].height = 1.0f;
+    state.objects[43].depth = 0.2f;
+
+    state.objects[44].scale = glm::vec3(8.0f, 1.0f, 0.2f);
+    state.objects[44].position = glm::vec3(-6.5f, 0.5f, -2.5f);   // 1st wall connected to start left wall
+    state.objects[44].width = 8.0f;
+    state.objects[44].height = 1.0f;
+    state.objects[44].depth = 0.2f;
+
+    state.objects[45].scale = glm::vec3(0.2f, 1.0f, 8.0f);
+    state.objects[45].position = glm::vec3(-10.5f, 0.5f, -6.5f);   // vertical wall, connect to 44
+    state.objects[45].width = 0.2f;
+    state.objects[45].height = 1.0f;
+    state.objects[45].depth = 8.0f;
+
+    // 'gates'
+    state.objects[46].scale = glm::vec3(8.0f, 1.0f, 0.2f);
+    state.objects[46].position = glm::vec3(-6.5f, 0.5f, -10.5f);   // horizontal wall between door wall and player wall
+    state.objects[46].width = 8.0f;
+    state.objects[46].height = 1.0f;
+    state.objects[46].depth = 0.2f;
+
+    state.objects[47].scale = glm::vec3(9.0f, 1.0f, 0.2f);
+    state.objects[47].position = glm::vec3(4.5f, 0.5f, -10.5f);   // horizontal wall on same horizonal as 46, has gap in between
+    state.objects[47].width = 9.0f;
+    state.objects[47].height = 1.0f;
+    state.objects[47].depth = 0.2f;
+
+    state.objects[48].scale = glm::vec3(0.2f, 1.0f, 9.0f);
+    state.objects[48].position = glm::vec3(-5.5f, 0.5f, -15.0f);   // vertical wall, connect to 44
+    state.objects[48].width = 0.2f;
+    state.objects[48].height = 1.0f;
+    state.objects[48].depth = 9.0f;
+
+    // connect gates to Top Left corner, gap in between for middle path
+
+    state.objects[49].scale = glm::vec3(0.2f, 1.0f, 6.0f);
+    state.objects[49].position = glm::vec3(-9.0f, 0.5f, -18.0f);   // vertical wall, connect to 44, 
+    state.objects[49].width = 0.2f;
+    state.objects[49].height = 1.0f;
+    state.objects[49].depth = 6.0f;
+
+    state.objects[50].scale = glm::vec3(12.0f, 1.0f, 0.2f);
+    state.objects[50].position = glm::vec3(-8.5f, 0.5f, 2.5f);   // horizontal wall, dead end
+    state.objects[50].width = 12.0f;
+    state.objects[50].height = 1.0f;
+    state.objects[50].depth = 0.2f;
+
+    state.objects[51].scale = glm::vec3(0.2f, 1.0f, 5.0f);
+    state.objects[51].position = glm::vec3(-14.5f, 0.5f, 0.0f);   // vertical wall
+    state.objects[51].width = 0.2f;
+    state.objects[51].height = 1.0f;
+    state.objects[51].depth = 5.0f;
+
+    //state.objects[52].textureID = ceilingTextureID;
+    state.objects[52].scale = glm::vec3(10.0f, 1.0f, 0.2f);
+    state.objects[52].position = glm::vec3(-1.0f, 0.5f, 15.0f);   // horizontal wall, dead end, connect 2 verticals, map back
+    state.objects[52].width = 10.0f;
+    state.objects[52].height = 1.0f;
+    state.objects[52].depth = 0.2f;
+
+    //state.objects[53].textureID = ceilingTextureID;
+    state.objects[53].scale = glm::vec3(8.0f, 1.0f, 0.2f);
+    state.objects[53].position = glm::vec3(5.0f, 0.5f, 10.0f);   // horizontal wall, dead end, connect 2 verticals, map back
+    state.objects[53].width = 8.0f;
+    state.objects[53].height = 1.0f;
+    state.objects[53].depth = 0.2f;
+
+    //state.objects[54].textureID = ceilingTextureID;
+    state.objects[54].scale = glm::vec3(0.2f, 1.0f, 4.0f);
+    state.objects[54].position = glm::vec3(2.5f, 0.5f, 4.0f);   // vertical wall, connect to 44
+    state.objects[54].width = 0.2f;
+    state.objects[54].height = 1.0f;
+    state.objects[54].depth = 4.0f;
+
+    //state.objects[55].textureID = ceilingTextureID;
+    state.objects[55].scale = glm::vec3(8.0f, 1.0f, 0.2f);
+    state.objects[55].position = glm::vec3(6.5f, 0.5f, 4.0f);   // horizontal wall, connect to entrence trick
+    state.objects[55].width = 8.0f;
+    state.objects[55].height = 1.0f;
+    state.objects[55].depth = 0.2f;
+
+    //state.objects[56].textureID = ceilingTextureID;
+    state.objects[56].scale = glm::vec3(0.2f, 1.0f, 4.0f);
+    state.objects[56].position = glm::vec3(10.5f, 0.5f, 4.0f);   // vertical wall, connect entrence trick and 55
+    state.objects[56].width = 0.2f;
+    state.objects[56].height = 1.0f;
+    state.objects[56].depth = 4.0f;
+
+    //state.objects[57].textureID = ceilingTextureID;
+    state.objects[57].scale = glm::vec3(0.2f, 1.0f, 3.0f);
+    state.objects[57].position = glm::vec3(7.5f, 0.5f, 8.5f);   // vertical wall, connect entrence trick and 55
+    state.objects[57].width = 0.2f;
+    state.objects[57].height = 1.0f;
+    state.objects[57].depth = 3.0f;
+
+    //state.objects[58].textureID = ceilingTextureID;
+    state.objects[58].scale = glm::vec3(0.2f, 1.0f, 5.0f);
+    state.objects[58].position = glm::vec3(1.0f, 0.5f, 12.5f);   // vertical wall, connect entrence trick and 55
+    state.objects[58].width = 0.2f;
+    state.objects[58].height = 1.0f;
+    state.objects[58].depth = 5.0f;
+
+    //state.objects[59].textureID = ceilingTextureID;
+    state.objects[59].scale = glm::vec3(0.2f, 1.0f, 10.0f);
+    state.objects[59].position = glm::vec3(-3.0f, 0.5f, 10.0f);   // vertical wall, connect entrence trick and 55
+    state.objects[59].width = 0.2f;
+    state.objects[59].height = 1.0f;
+    state.objects[59].depth = 10.0f;
+
+    //state.objects[60].textureID = ceilingTextureID;
+    state.objects[60].scale = glm::vec3(0.2f, 1.0f, 5.0f);
+    state.objects[60].position = glm::vec3(-8.0f, 0.5f, 5.0f);   // vertical wall, connect entrence trick and 55
+    state.objects[60].width = 0.2f;
+    state.objects[60].height = 1.0f;
+    state.objects[60].depth = 5.0f;
+
 
 
     //// ----- HEALTH ----- 
